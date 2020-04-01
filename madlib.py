50dec4813eea9d6583948beffbfcb0c81c4dacfe
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-01 11:27:38 -0400

    completed madlib

---
+++ madlib.py
@@ -0,0 +1,4 @@
+print("Please fill in the blanks below:\n__(name)__'s favorite subject in school is __(subject)__.")
+name = input("What is name? ")
+subject = input("What is subject? ")
+print(f"{name}'s favorite subject in school is {subject}.")
