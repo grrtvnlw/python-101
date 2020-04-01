f4ca67d6c3c4d757b9916b9775aa0105471e4cdc
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-01 11:12:04 -0400

    inital commit

---
+++ example.py
@@ -0,0 +1,13 @@
+# Step 0: Document the problem
+# Add a couple of numbers and print it out
+
+# 1. Setup/Configuration
+result = 0 # give it a sane default
+num_1 = 10
+num_2 = 11
+
+#3. Do the work
+result = num_1 + num_2
+
+# 2. Result/Output
+print(result)
